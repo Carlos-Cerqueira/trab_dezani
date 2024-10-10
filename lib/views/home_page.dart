0949afa8379952a0a0eba9f76e6b6cb2b035fc0c
Carlos-Cerqueira <carlosleite.884@gmail.com>
2024-10-10 20:58:19 -0300

    Telas prontas, API bugada

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,35 +1,187 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import '../services/books_api.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final BooksApi _booksApi = BooksApi();
+  final TextEditingController _searchController = TextEditingController();
+  Map<String, dynamic>? _bookData;
+  bool _isLoading = false;
+  String _errorMessage = '';
+
+  Future<void> _searchBook() async {
+    setState(() {
+      _isLoading = true;
+      _errorMessage = '';
+    });
+
+    final data = await _booksApi.fetchBook(_searchController.text, limit: 10);
+
+    if (data != null && data['docs'] != null) {
+      setState(() {
+        _bookData = data;
+        _isLoading = false;
+      });
+    } else {
+      setState(() {
+        _errorMessage = 'Nenhum resultado encontrado. Tente outra pesquisa.';
+        _isLoading = false;
+      });
+    }
+  }
+
+  String _getCoverUrl(Map<String, dynamic> book) {
+    if (book['cover_i'] != null) {
+      return 'https://covers.openlibrary.org/b/id/${book['cover_i']}-L.jpg';
+    } else if (book['isbn'] != null && (book['isbn'] as List).isNotEmpty) {
+      return 'https://covers.openlibrary.org/b/isbn/${book['isbn'][0]}-L.jpg';
+    }
+    return ''; 
+  }
+
+  String _getValidData(dynamic data) {
+    return data != null ? data.toString() : 'N/A';
+  }
+
+  bool _isValidBook(Map<String, dynamic> book) {
+    return book['title'] != null &&
+        book['author_name'] != null &&
+        book['publisher'] != null &&
+        book['first_publish_year'] != null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('RateBooks'),
-      ),
-      body: Container(
-        width: double.infinity,
-        color: const Color(0xFFDFF0D8),
-        child: Column(
+        backgroundColor: const Color(0xFFF6E3CF),
+        title: Row(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Procure um livro!',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-        
-            Expanded(
-              child: Center(
-                child: Text('Nada por aqui! Tente pesquisar algum livro.'),
-              ),
+            Image.asset(
+              'assets/images/Logo.png',
+              height: 40,
             ),
           ],
+        ),
+      ),
+      body: Container(
+        color: const Color(0xFFF6E3CF),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  labelText: 'Pesquise por um livro',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  prefixIcon: Icon(Icons.search),
+                ),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _searchBook,
+                child: const Text('Buscar'),
+              ),
+              const SizedBox(height: 16),
+              if (_isLoading)
+                const CircularProgressIndicator()
+              else if (_errorMessage.isNotEmpty)
+                Center(
+                  child: Text(
+                    _errorMessage,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                )
+              else if (_bookData != null && _bookData!['docs'] != null)
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: _bookData!['docs'].length,
+                    itemBuilder: (context, index) {
+                      final book = _bookData!['docs'][index];
+        
+                      if (!_isValidBook(book)) {
+                        return const SizedBox.shrink();
+                      }
+        
+                      final coverUrl = _getCoverUrl(book);
+        
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.network(
+                                coverUrl.isNotEmpty
+                                    ? coverUrl
+                                    : 'assets/images/placeholder_cover.png',
+                                height: 150,
+                                width: 100,
+                                errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                                  return Image.asset(
+                                    'assets/images/placeholder_cover.png',
+                                    height: 150,
+                                    width: 100,
+                                  );
+                                },
+                              ),
+                              const SizedBox(width: 10),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Título: ${_getValidData(book['title'])}',
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    Text(
+                                      'Autores: ${_getValidData((book['author_name'] as List?)?.join(', '))}',
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    Text(
+                                      'Editora: ${_getValidData((book['publisher'] as List?)?.first)}',
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    Text(
+                                      'Data de publicação: ${_getValidData(book['first_publish_year'])}',
+                                    ),
+                                    Text(
+                                      'Número de páginas: ${_getValidData(book['number_of_pages_median'])}',
+                                    ),
+                                    Text(
+                                      'ISBN: ${_getValidData((book['isbn'] as List?)?.first)}',
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                )
+              else
+                const Center(
+                  child: Text(
+                    'Nenhum resultado encontrado.',
+                    style: TextStyle(fontSize: 16, color: Colors.black54),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomAppBar(
@@ -85,7 +237,7 @@ class HomePage extends StatelessWidget {
                 height: 40,
               ),
               onPressed: () {
-                Navigator.pushReplacementNamed(context, '/current'); 
+                Navigator.pushReplacementNamed(context, '/current');
               },
             ),
           ],
--- lib/views/lists_page.dart
+++ lib/views/lists_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 
@@ -6,19 +6,38 @@ class ListsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFDFF0D8),
       appBar: AppBar(
-        backgroundColor: Color(0xFFDFF0D8),
-        title: Text('Minhas Listas'),
+        backgroundColor: const Color(0xFFDFF0D8),
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/images/Logo.png',
+              height: 40,
+            ),
+          ],
+        ),
       ),
       body: Container(
         width: double.infinity,
         color: const Color(0xFFDFF0D8),
         child: SingleChildScrollView(
-          child: Center(
-            child: Text('Lista de Livros Lidos, Quero Ler, etc.'),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildSectionTitle('Lendo'),
+              _buildBookList(),
+              SizedBox(height: 20),
+              _buildSectionTitle('Lidos'),
+              _buildBookList(),
+              SizedBox(height: 20),
+              _buildSectionTitle('Quero Ler'),
+              _buildBookList(),
+              SizedBox(height: 20),
+              _buildSectionTitle('Avaliados'),
+              _buildBookList(),
+            ],
           ),
-          
         ),
       ),
       bottomNavigationBar: BottomAppBar(
@@ -82,4 +101,70 @@ class ListsPage extends StatelessWidget {
       ),
     );
   }
+
+  Widget _buildSectionTitle(String title) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 1.0),
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black87,
+            ),
+          ),
+        ),
+        Divider(
+          color: Colors.grey[400],
+          thickness: 1,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBookList() {
+    return Container(
+      height: 150,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.grey[300],
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/placeholder_cover.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  'O Pequeno Príncipe',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
+
